ac86156a0f9e5851c083e539ca9a4cb91100148a
Your Name <your-email@example.com>
2025-02-28 12:09:26 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 // src/App.jsx
 
+// src/App.jsx
+
 import React from 'react';
 import Home from './Home';
 import './App.css'; // Import global styles
-
+import { Routes, Route } from "react-router-dom";
+import AddBook from "./AddBook";
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Routes>
+        <Route path ='/'element={<Home />}/>
+        <Route path="/add-book"element={<AddBook/>}/>
+      </Routes>
+
+
     </div>
   );
 }
